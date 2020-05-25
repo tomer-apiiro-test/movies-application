78d23b5a36f519ccfd5dd07a918b8f736cb4560c
Tomer Amir <tomer@apiiro.com>
2020-05-25 19:40:48 +0300

    first commit

---
+++ src/main/java/com/tomer/springdemo/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.tomer.springdemo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping(method = RequestMethod.GET, path = "/")
+    @ResponseBody
+    public String test() {
+        return "follow the white rabbit...";
+    }
+}
