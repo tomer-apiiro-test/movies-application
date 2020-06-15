5e46274dc0aab99d25228a0254bbb1ecb0b4d3b9
Tomer Amir <tomer@apiiro.com>
2020-06-15 13:42:06 +0300

    Fix LOTRController

--- src/main/java/com/tomer/springdemo/controllers/LOTRController.java
+++ src/main/java/com/tomer/springdemo/controllers/LOTRController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/lotr")
+@RestController
+@RequestMapping("/lotr")
 public class LOTRController {
     @RequestMapping()
     @ResponseBody
